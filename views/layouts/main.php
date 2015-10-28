94a511360b1c618f29f1f7808539ada19eb14aa4
ansonmouse <ansonmouse@me.com>
2015-10-28 08:24:50 +0800

    Update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'COMP3421',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -61,9 +61,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; COMP3421 Project Team 24 <?= date('Y') ?> • All rights reserved</p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?php //Yii::powered() ?></p>
     </div>
 </footer>
 
