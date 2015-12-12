11e7dc4adbfee352b0775fbc7c7a81cb5c9b63bf
ansonmouse <ansonmouse@me.com>
2015-12-12 16:08:42 +0800

    Minor Update on Nav Bar

--- views/post/index.php
+++ views/post/index.php
@@ -10,7 +10,7 @@ use yii\grid\GridView;
 $this->title = 'Posts';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="post-index">
+<div class="post-index container">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -19,20 +19,35 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <table class="table">
+        <tr>
+            <th>Topic</th>
+            <th>Category</th>
+            <th>User</th>
+            <th>Replies</th>
+            <th>Activity</th>
+        </tr>
+    </table>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'title',
+            [
+                'attribute'=>'title',
+                'label'=>'Topic',
+                'format'=>'raw',
+                'value'=>function($data) {return Html::a($data->title, ['post/view', 'id'=>$data->id]);}
+            ],
             'content',
-            'datetime',
+            [
+                'attribute'=>'datetime',
+                'label'=>'Activity',
+                'format'=>'text',
+                'value'=>function($date) {return date_diff(date_create($date->datetime), date_create());}
+            ],
             'Participant_id',
             // 'Topic_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 
