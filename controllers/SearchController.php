2b3c83e65bdbaa33c5232e77f366973b95339431
Anson <ansonmouse@me.com>
2015-12-12 14:18:39 +0800

    Update on search

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\ActivitySearchGlobal;
 use app\models\OutsideAttractionSearchGlobal;
+use app\models\PostSearchGlobal;
 use yii\web\ForbiddenHttpException;
 use yii\helpers\BaseArrayHelper;
 
@@ -22,6 +23,12 @@ class SearchController extends \yii\web\Controller
             $outdoorAttractionDataProvider = $outdoorAttractionSearchModel->search(Yii::$app->request->queryParams);
 
             $dataProvider = BaseArrayHelper::merge($activitySearchModelDataProvider, $outdoorAttractionDataProvider);
+
+            $postSearchModel = new PostSearchGlobal();
+            $postSearchModel->globalSearch = $search;
+            $postDataProvider = $postSearchModel->search(Yii::$app->request->queryParams);
+
+            $dataProvider = BaseArrayHelper::merge($dataProvider, $postDataProvider);
 
             return $this->render('index', [
                 'input' => $search,
