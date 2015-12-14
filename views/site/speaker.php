e638fed13ce7411db6da479096144c16c86aacda
fionachy <13069036d@connect.polyu.hk>
2015-12-14 22:29:20 +0800

    updated speaker.php

--- views/site/speaker.php
+++ views/site/speaker.php
@@ -7,36 +7,45 @@ use yii\helpers\Html;
 $this->title = 'Speaker';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+<link href="/assets/css/fonts/pe-icon-7-stroke.css" rel="stylesheet">
+<link href="/assets/css/fonts/Rubik-Fonts.css" rel="stylesheet" />
+
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
 
     <div class="section section-team-2" id="team">
-        <div class="container">
-            <div class="text-area">
-            </div>
+
+
             <div class="team">
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-1">
-                            <img class="img-circle" src="assets/img/elena.jpg"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/ng-vincent.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Elena Conacel </h3>
-                                <br>
+                                <h3 class="big-text">Dr NG To Yee Vincent</h3>
+                                <h5>Topic: Emotion Discovery with your FB photos</h5>
+                                <h5>Research Interest: MOOC for school students</h5>
                                 <p class="small-text">Elena takes care of growth, product development and partnerships. She is analytical, driven and adapts easily to new conditions, while trying to keep everyone on track.
                                 </p>
                             </div>
                             <div class="social-buttons hidden-xs">
+                                <a target="_blank" href="https://www.facebook.com/axelut?fref=ts" class="btn btn-social btn-facebook btn-simple"><i class="fa fa-facebook-square"></i></a>
                                 <a target="_blank" href="https://twitter.com/conacelelena" class="btn btn-social btn-twitter btn-simple"><i class="fa fa-twitter"></i></a>
                                 <a target="_blank" href="https://www.linkedin.com/profile/view?id=250414665&trk=hp-identity-photo" class="btn btn-social btn-linkedin btn-simple"><i class="fa fa-linkedin-square"></i></a>                                    </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <br>
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-2">
-                            <img class="img-circle" src="assets/img/alex.png"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/memory.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Alex Paduraru</h3>
-                                <br>
+                                <h3 class="big-text">Ms CHIU Wai Han Memory</h3>
+                                <h5>Topic: Playful interaction for social interaction through web applications</h5>
+                                <h5>Research Interest: Database systems, Web Application Systems</h5>
                                 <p class="small-text">Alex drives the technical strategy of the platform, customer support and brand. He is level-headed, smart and with a taste for simple, aesthetic design.</p>
                             </div>
                             <div class="social-buttons hidden-xs">
@@ -46,13 +55,14 @@ $this->params['breadcrumbs'][] = $this->title;
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-md-4">
+                    <br>
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-2">
-                            <img class="img-circle" src="assets/img/alex.png"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/Takazumi.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Alex Paduraru</h3>
-                                <br>
+                                <h3 class="big-text">Dr MATSUMOTO Takazumi</h3>
+                                <h5>Topic: Application for group involvement in mass lecture</h5>
+                                <h5>Research Interest: Application for multiple users account in one mobile</h5>
                                 <p class="small-text">Alex drives the technical strategy of the platform, customer support and brand. He is level-headed, smart and with a taste for simple, aesthetic design.</p>
                             </div>
                             <div class="social-buttons hidden-xs">
@@ -62,13 +72,14 @@ $this->params['breadcrumbs'][] = $this->title;
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-md-4">
+                    <br>
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-2">
-                            <img class="img-circle" src="assets/img/alex.png"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/lu-qin.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Alex Paduraru</h3>
-                                <br>
+                                <h3 class="big-text">Prof. LU Qin</h3>
+                                <h5>Topic: Emotion analysis of text</h5>
+                                <h5>Research Interest: Develop a assistive software for students with dyslexia</h5>
                                 <p class="small-text">Alex drives the technical strategy of the platform, customer support and brand. He is level-headed, smart and with a taste for simple, aesthetic design.</p>
                             </div>
                             <div class="social-buttons hidden-xs">
@@ -78,14 +89,14 @@ $this->params['breadcrumbs'][] = $this->title;
                             </div>
                         </div>
                     </div>
-
-
-                    <div class="col-md-4">
+                    <br>
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-2">
-                            <img class="img-circle" src="assets/img/alex.png"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/rocky.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Alex Paduraru</h3>
-                                <br>
+                                <h3 class="big-text">Dr CHANG Kow Chuen Rocky</h3>
+                                <h5>Topic: Measuring the quality of experience of mobile network services</h5>
+                                <h5>Research Interest: Sealing Privacy leaks in web browsers</h5>
                                 <p class="small-text">Alex drives the technical strategy of the platform, customer support and brand. He is level-headed, smart and with a taste for simple, aesthetic design.</p>
                             </div>
                             <div class="social-buttons hidden-xs">
@@ -95,13 +106,14 @@ $this->params['breadcrumbs'][] = $this->title;
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-md-4">
+                    <br>
+                    <div class="col-md-4" align="center">
                         <div class="member add-animation animation-2">
-                            <img class="img-circle" src="assets/img/alex.png"/>
+                            <img class="img-circle" src="/comp3421/web/../resources/speaker/peter.jpg"/>
                             <div class="description">
-                                <h3 class="big-text">Alex Paduraru</h3>
-                                <br>
+                                <h3 class="big-text">Dr NG Hiu Fung Peter</h3>
+                                <h5>Topic: Virtual and Augmented Reality Development</h5>
+                                <h5>Research Interest: Aesthetic modifier for real object</h5>
                                 <p class="small-text">Alex drives the technical strategy of the platform, customer support and brand. He is level-headed, smart and with a taste for simple, aesthetic design.</p>
                             </div>
                             <div class="social-buttons hidden-xs">
@@ -114,7 +126,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 </div>
             </div>
-        </div>
+
     </div>
 
 
