2ee5661baeaa639050000168564da5faa72d9319
Anson <ansonmouse@me.com>
2015-12-20 05:15:03 +0800

    Forum

--- views/post/index.php
+++ views/post/index.php
@@ -20,7 +20,7 @@ $this->title = 'Forum';
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        //'filterModel' => $searchModel,
         'columns' => [
             [
                 'attribute'=>'title',
