666e81f668ecb6fbf5440c0525508618354eff80
Anson <ansonmouse@me.com>
2015-12-20 09:48:47 +0800

    db

--- config/db.php
+++ config/db.php
@@ -8,19 +8,19 @@
     'charset' => 'utf8',
 ];*/
 
-return [
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=mysql.comp.polyu.edu.hk;dbname=13027272d',
     'username' => '13027272d',
     'password' => 'rviwwmss',
     'charset' => 'utf8',
-];
+];*/
 
 
-/*return [
+return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=13027272d',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-];*/
+];
